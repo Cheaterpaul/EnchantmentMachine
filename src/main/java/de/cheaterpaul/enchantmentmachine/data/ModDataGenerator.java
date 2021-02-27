5e3e3f502f6498533808f6632ed464d87f3901eb
maxanier <dev@maxgb.de>
2021-02-27 20:08:17 +0100

    Add drops for (dis)enchanter

--- src/main/java/de/cheaterpaul/enchantmentmachine/data/ModDataGenerator.java
+++ src/main/java/de/cheaterpaul/enchantmentmachine/data/ModDataGenerator.java
@@ -1,24 +1,29 @@
 package de.cheaterpaul.enchantmentmachine.data;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
 import de.cheaterpaul.enchantmentmachine.core.ModData;
 import de.cheaterpaul.enchantmentmachine.util.REFERENCE;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.renderer.model.BlockModel;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
-import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.data.*;
+import net.minecraft.data.loot.BlockLootTables;
 import net.minecraft.item.Items;
-import net.minecraft.util.Direction;
+import net.minecraft.loot.*;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class ModDataGenerator {
 
@@ -72,12 +77,46 @@ public class ModDataGenerator {
         }
     }
 
+
     public static void gatherData(final GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         if (event.includeClient()) {
             generator.addProvider(new BlockStateGenerator(generator, event.getExistingFileHelper()));
             generator.addProvider(new ItemModelGenerator(generator, event.getExistingFileHelper()));
         }
+        generator.addProvider(new ModLootTableProvider(generator));
         generator.addProvider(new RecipeGenerator(generator));
+    }
+
+    private static class ModLootTableProvider extends LootTableProvider {
+
+        public ModLootTableProvider(DataGenerator dataGeneratorIn) {
+            super(dataGeneratorIn);
+        }
+
+        @Override
+        protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
+            return ImmutableList.of(Pair.of(Tables::new, LootParameterSets.BLOCK));
+        }
+
+        @Override
+        protected void validate(Map<ResourceLocation, LootTable> map, ValidationTracker validationtracker) {
+            map.forEach((resourceLocation, lootTable) -> LootTableManager.validateLootTable(validationtracker, resourceLocation, lootTable));
+        }
+
+        private static class Tables extends BlockLootTables {
+            @Override
+            protected void addTables() {
+                this.registerDropSelfLootTable(ModData.disenchanter_block);
+                this.registerDropSelfLootTable(ModData.enchanter_block);
+                this.registerDropSelfLootTable(ModData.enchantment_block);
+            }
+
+            @Nonnull
+            @Override
+            protected Iterable<Block> getKnownBlocks() {
+                return Lists.newArrayList(ModData.disenchanter_block, ModData.enchanter_block, ModData.enchantment_block);
+            }
+        }
     }
 }
