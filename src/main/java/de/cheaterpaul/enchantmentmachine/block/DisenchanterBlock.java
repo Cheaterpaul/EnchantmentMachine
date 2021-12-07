f81c4bcbd5f3c6f51eb6fd556b2ff49ade5976df
cheaterpaul <dev@paube.de>
2021-12-07 18:37:42 +0100

    add item tooltip of block placing

--- src/main/java/de/cheaterpaul/enchantmentmachine/block/DisenchanterBlock.java
+++ src/main/java/de/cheaterpaul/enchantmentmachine/block/DisenchanterBlock.java
@@ -2,10 +2,15 @@ package de.cheaterpaul.enchantmentmachine.block;
 
 import de.cheaterpaul.enchantmentmachine.block.entity.DisenchanterBlockEntity;
 import de.cheaterpaul.enchantmentmachine.core.ModData;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -17,9 +22,11 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class DisenchanterBlock extends EnchantmentBaseBlock {
 
@@ -45,6 +52,13 @@ public class DisenchanterBlock extends EnchantmentBaseBlock {
             return InteractionResult.CONSUME;
         }
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack itemStack, @Nullable BlockGetter p_49817_, @NotNull List<Component> tooltips, @NotNull TooltipFlag flag) {
+        super.appendHoverText(itemStack, p_49817_, tooltips, flag);
+        tooltips.add(new TranslatableComponent("text.enchantmentmachine.next_to_storage_block", ModData.storage_block.getName()).withStyle(ChatFormatting.GRAY));
+
     }
 
     @SuppressWarnings("deprecation")
--- src/main/java/de/cheaterpaul/enchantmentmachine/block/EnchanterBlock.java
+++ src/main/java/de/cheaterpaul/enchantmentmachine/block/EnchanterBlock.java
@@ -5,11 +5,16 @@ import de.cheaterpaul.enchantmentmachine.block.entity.EnchanterBlockEntity;
 import de.cheaterpaul.enchantmentmachine.block.entity.StorageBlockEntity;
 import de.cheaterpaul.enchantmentmachine.core.ModData;
 import de.cheaterpaul.enchantmentmachine.network.message.EnchantmentPacket;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -19,8 +24,11 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Optional;
 
 public class EnchanterBlock extends EnchantmentBaseBlock {
@@ -56,6 +64,13 @@ public class EnchanterBlock extends EnchantmentBaseBlock {
             return InteractionResult.CONSUME;
         }
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack itemStack, @Nullable BlockGetter p_49817_, @NotNull List<Component> tooltips, @NotNull TooltipFlag flag) {
+        super.appendHoverText(itemStack, p_49817_, tooltips, flag);
+        tooltips.add(new TranslatableComponent("text.enchantmentmachine.next_to_storage_block", ModData.storage_block.getName()).withStyle(ChatFormatting.GRAY));
+
     }
 
     @SuppressWarnings("deprecation")
